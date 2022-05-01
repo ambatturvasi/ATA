e297828d4b58d9e270221afba870ba780da87047
team ambatturvasi <teamambatturvasi@gmail.com>
2022-05-01 18:19:10 +0530

    new1

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,86 +1,21 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native-web";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
-import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
-import {Icon} from 'react-native-elements';
-
-import HomeScreen from './HomeScreen';
-import TravelScreen from './src/screens/TravelScreen';
-import ServiceScreen from './src/screens/ServiceScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-
-
-class HomeScreen extends Component
-{
-    render()
-    {
-        return(
-            <View style={{flex:1, justifyContent:'center' , alignItem: 'center'}}>
-                <Text style={{fontSize:40}}>Home Screen</Text>
-            </View>
-        );
-    }
+export default function ServiceScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Service Screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-const TabNavigator=createMaterialBottomTabNavigator(
-    {
-        Home :{screen:HomeScreen,
-            navigationOptions:{
-                tabBarLabel:'Home',
-                activeColor:'#ff0000',
-                inactiveColor:'#0000000',
-                barStyle:{backgroundColor:'#67baf6'},
-                tabBarIcon:()=>(
-                    <View>
-                        <Icon name={'home'} size={25} style={{color:'#ff0000'}}/>
-                    </View>
-                )
-            }
-        },
-
-        Travel :{screen:TravelScreen,
-            navigationOptions:{
-                tabBarLabel:'Home',
-                activeColor:'#ff0000',
-                inactiveColor:'#0000000',
-                barStyle:{backgroundColor:'#67baf6'},
-                tabBarIcon:()=>(
-                    <View>
-                        <Icon name={'home'} size={25} style={{color:'#ff0000'}}/>
-                    </View>
-                )
-            }
-        },
-
-        Service :{screen:ServiceScreen,
-            navigationOptions:{
-                tabBarLabel:'Home',
-                activeColor:'#ff0000',
-                inactiveColor:'#0000000',
-                barStyle:{backgroundColor:'#67baf6'},
-                tabBarIcon:()=>(
-                    <View>
-                        <Icon name={'home'} size={25} style={{color:'#ff0000'}}/>
-                    </View>
-                )
-            }
-        },
-
-        Profile :{screen:ProfileScreen,
-            navigationOptions:{
-                tabBarLabel:'Home',
-                activeColor:'#ff0000',
-                inactiveColor:'#0000000',
-                barStyle:{backgroundColor:'#67baf6'},
-                tabBarIcon:()=>(
-                    <View>
-                        <Icon name={'person'} size={25} style={{color:'#ff0000'}}/>
-                    </View>
-                )
-            }
-        }
-    }
-);
-
-export default createAppContainer(TabNavigator);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/screens/TravelScreen.js
+++ src/screens/TravelScreen.js
@@ -1,167 +1,21 @@
-import React from 'react'
-import {
-  FlatList,
-  View,
-  ActivityIndicator,
-  TouchableOpacity
-} from 'react-native'
-import filter from 'lodash.filter'
-import { ApplicationProvider, Text, Avatar, Input } from '@ui-kitten/components'
-import { mapping, light as lightTheme } from '@eva-design/eva'
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-class HomeScreen extends React.Component {
-  state = {
-    loading: false,
-    data: [],
-    page: 1,
-    seed: 1,
-    error: null,
-    query: '',
-    fullData: []
-  }
-
-  componentDidMount() {
-    this.makeRemoteRequest()
-  }
-
-  makeRemoteRequest = () => {
-    const { page, seed } = this.state
-    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`
-    this.setState({ loading: true })
-
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          data: page === 1 ? res.results : [...this.state.data, ...res.results],
-          error: res.error || null,
-          loading: false,
-          fullData: res.results
-        })
-      })
-      .catch(error => {
-        this.setState({ error, loading: false })
-      })
-  }
-
-  contains = ({ name, email }, query) => {
-    const { first, last } = name
-    if (
-      first.includes(query) ||
-      last.includes(query) ||
-      email.includes(query)
-    ) {
-      return true
-    }
-    return false
-  }
-
-  handleSearch = text => {
-    const formattedQuery = text.toLowerCase()
-    const data = filter(this.state.fullData, user => {
-      return this.contains(user, formattedQuery)
-    })
-    this.setState({ data, query: text })
-  }
-
-  renderHeader = () => (
-    <View
-      style={{
-        backgroundColor: '#fff',
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}>
-      <Input
-        autoCapitalize='none'
-        autoCorrect={false}
-        onChangeText={this.handleSearch}
-        status='info'
-        placeholder='Search'
-        style={{
-          borderRadius: 25,
-          borderColor: '#333',
-          backgroundColor: '#fff'
-        }}
-        textStyle={{ color: '#000' }}
-        clearButtonMode='always'
-      />
+export default function ServiceScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Service Screen</Text>
+      <StatusBar style="auto" />
     </View>
-  )
-
-  renderSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: '86%',
-          backgroundColor: '#CED0CE',
-          marginLeft: '5%'
-        }}
-      />
-    )
-  }
-
-  renderFooter = () => {
-    if (!this.state.loading) return null
-    return (
-      <View
-        style={{
-          paddingVertical: 20,
-          borderTopWidth: 1,
-          borderColor: '#CED0CE'
-        }}>
-        <ActivityIndicator animating size='large' />
-      </View>
-    )
-  }
-
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          paddingHorizontal: 20,
-          paddingVertical: 20,
-          marginTop: 40
-        }}>
-        <FlatList
-          data={this.state.data}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => alert('Item pressed!')}>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  padding: 16,
-                  alignItems: 'center'
-                }}>
-                <Avatar
-                  source={{ uri: item.picture.thumbnail }}
-                  size='giant'
-                  style={{ marginRight: 16 }}
-                />
-                <Text
-                  category='s1'
-                  style={{
-                    color: '#000'
-                  }}>{`${item.name.first} ${item.name.last}`}</Text>
-              </View>
-            </TouchableOpacity>
-          )}
-          keyExtractor={item => item.email}
-          ItemSeparatorComponent={this.renderSeparator}
-          ListHeaderComponent={this.renderHeader}
-          ListFooterComponent={this.renderFooter}
-        />
-      </View>
-    )
-  }
+  );
 }
 
-const App = () => (
-  <ApplicationProvider mapping={mapping} theme={lightTheme}>
-    <HomeScreen />
-  </ApplicationProvider>
-)
-
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
